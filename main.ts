6bb7a2f36c40dfe5131dd20416d59c2ea01fad38
jordicollell <78138546+jordicollell@users.noreply.github.com>
2022-02-11 00:17:47 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,13 @@
 let LLUM = 0
 input.onButtonPressed(Button.A, function () {
+    music.playTone(3000, music.beat(BeatFraction.Sixteenth))
     LLUM += 10
     if (LLUM > 255) {
         LLUM = 255
     }
 })
 input.onButtonPressed(Button.B, function () {
+    music.playTone(700, music.beat(BeatFraction.Sixteenth))
     LLUM += -10
     if (LLUM < 0) {
         LLUM = 0
